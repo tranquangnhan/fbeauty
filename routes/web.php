a97ef8edebae23be319ed6255cf2ed7462f80c6f
Quangnhankie <nhantran@kie.io>
2021-09-26 11:01:44 +0700

    feat: create controller dangnhap

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DangNhapAdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/admin/dangnhapadmin', [\App\Http\Controllers\DangNhapAdminController::class, 'login']);
+
+Route::group(['prefix' => 'quantri'], function (){
+    
+
+
+
 });
