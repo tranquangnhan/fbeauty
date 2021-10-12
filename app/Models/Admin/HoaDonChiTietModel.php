85387ae8aa6186830f1c8055534b0a3eb47ec519
Ngô Anh Quốc <quocna@sangiare.vn>
2021-10-12 22:07:54 +0700

    lich

---
+++ app/Models/Admin/HoaDonChiTietModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HoaDonChiTietModel extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Admin/HoaDonModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HoaDonModel extends Model
+{
+    use HasFactory;
+}
