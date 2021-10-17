fec95bd79bfe136c2a8be840b42ae80e1573bcf7
Quangnhankie <nhantran@kie.io>
2021-10-17 15:28:49 +0700

    lieutrinh

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,12 @@ use App\Http\Controllers\Admin\HoaDonChiTietController;
 use App\Http\Controllers\Admin\HoaDonController;
 use App\Http\Controllers\Admin\KhachHangController;
 use App\Http\Controllers\Admin\LichController;
+use App\Http\Controllers\Admin\LieuTrinhController;
 use App\Http\Controllers\Admin\SanPhamController;
 use App\Http\Controllers\Admin\NhanVienController;
 use App\Http\Controllers\Admin\SanPhamChiTietController;
 use App\Http\Controllers\Admin\ThongkeController;
+
 use App\Http\Controllers\Site\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -72,10 +74,12 @@ Route::group(['prefix' => 'quantri', 'middleware' => 'phanquyen'], function (){
     Route::resource('coso', CoSoController::class);
     Route::post('/select-delivery', [CoSoController::class,'select_delivery']);
 
-     Route::resource('donhang', DonHangController::class);
+    Route::resource('donhang', DonHangController::class);
     Route::get('/active/{id}', [DonHangController::class,'active']);
     Route::get('/active-1/{id}', [DonHangController::class,'active_1']);
     Route::get('/active-2/{id}', [DonHangController::class,'active_2']);
+
+    Route::resource('lieutrinh', LieuTrinhController::class);
 });
 
 Route::group(['prefix' => '/'], function (){
