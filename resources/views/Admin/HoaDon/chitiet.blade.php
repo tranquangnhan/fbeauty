abf11c151c5e7e5320fb8af4f5f04b0fcba57e2b
Ngô Anh Quốc <quocna@sangiare.vn>
2021-10-29 23:13:39 +0700

    xoa submit

--- resources/views/Admin/HoaDon/chitiet.blade.php
+++ resources/views/Admin/HoaDon/chitiet.blade.php
@@ -142,7 +142,6 @@
                                 <div class="d-print-none">
                                     <div class="float-right">
                                         <a href="javascript:window.print()" class="btn btn-dark waves-effect waves-light"><i class="fa fa-print"></i></a>
-                                        <a href="#" class="btn btn-primary waves-effect waves-light">Submit</a>
                                     </div>
                                     <div class="clearfix"></div>
                                 </div>
