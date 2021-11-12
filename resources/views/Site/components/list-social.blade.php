c5969ecfc35525a361cac4c92594a7c6b6a20b8c
long.nh <long.nh2401@gmail.com>
2021-11-12 08:13:15 +0700

    profile user

--- resources/views/Site/components/list-social.blade.php
+++ resources/views/Site/components/list-social.blade.php
@@ -1,42 +1,48 @@
-<div class="title-box-2">
-    <span>Mạng xã hội</span>
-</div>
+<div class="box-list-cocial">
+    <div class="title-box-2">
+        <span>Mạng xã hội</span>
+    </div>
 
-<div class="list-social">
-    <a href="#">
-        <span><i class="fab fa-facebook-f"></i> <span>15000</span> <small class="fz-0em9"> Like</small></span>
-        <span class="fz-0em9">Like</span>
-    </a>
+    <div class="list-social">
+        <a href="#">
+            <span><i class="fab fa-facebook-f"></i> <span>15000</span></span>
+            <span class="fz-0em9">Like</span>
+        </a>
 
-    <a href="#">
-        <span><i class="fab fa-twitter"></i> <span>15000</span> <small class="fz-0em9"> Tweet</small></span>
-        <span class="fz-0em9">Like</span>
-    </a>
+        <a href="#">
+            <span><i class="fab fa-twitter"></i> <span>15000</span></span>
+            <span class="fz-0em9">Like</span>
+        </a>
 
-    <a href="#">
-        <span><i class="fab fa-instagram"></i><span>32k+</span> <small class="fz-0em9"> Follower</small></span>
-        <span class="fz-0em9">Follower</span>
-    </a>
+        <a href="#">
+            <span><i class="fab fa-instagram"></i><span>32k+</span></span>
+            <span class="fz-0em9">Follower</span>
+        </a>
 
-    <a href="#">
-        <span><i class="fab fa-youtube"></i> <span>15000</span> <small class="fz-0em9"> Subscribe</small></span>
-        <span class="fz-0em9">Subscribe</span>
-    </a>
-</div>
+        <a href="#">
+            <span><i class="fab fa-youtube"></i> <span>15000</span></span>
+            <span class="fz-0em9">Subscribe</span>
+        </a>
+    </div>
 
-<div class="datlich mt-4">
-    <div class="child-box-tin-2 d-flex align-items-center box-small" style="background: url('{{ asset('Site/images') }}/toa-heftiba-a9pFSC8dTlo-unsplash.jpg');">
-        <div class="content-tin background-white">
-            <div class="text-bl-1 limit-text-row-2 mb-2">
-                <a href="" class="color-white hover-pink">Trải nghiệm cảm giác spa <br> đỉnh cao tại Fbeauty spa</a>
-            </div>
-            <div class="row">
-                <div class="col-xl-12 d-flex align-items-center color-gray-2 hover-color-black text-small-1">
-                    <div class="mr-3">
-                        <a href="" class="box-danhmuc-1 maincolor">Đặt lịch</a>
+    <div class="datlich mt-4">
+        <div class="child-box-tin-2 d-flex align-items-center box-small mb-1"
+            style="background: url('{{ asset('Site/images') }}/toa-heftiba-a9pFSC8dTlo-unsplash.jpg');">
+            <div class="content-tin background-white">
+                <div class="text-bl-1 limit-text-row-2 mb-2">
+                    <a href="" class="color-white hover-pink">Trải nghiệm cảm giác spa <br> đỉnh cao tại
+                        Fbeauty spa</a>
+                </div>
+                <div class="row">
+                    <div
+                        class="col-xl-12 d-flex align-items-center color-gray-2 hover-color-black text-small-1 mb-1">
+                        <div class="mr-3">
+                            <a href="" class="box-danhmuc-1 maincolor">Đặt lịch</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 </div>
